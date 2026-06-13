eddb74393312d946a0b89bbdefbffe8b0278eb24
Lucas Alves <lucasacastela@gmail.com>
2026-06-13 14:20:38 -0300

    Resolvendo problema de conexão

--- src/main/java/com/example/demo/Conexao.java
+++ src/main/java/com/example/demo/Conexao.java
@@ -8,6 +8,7 @@ public class Conexao {
 
     private static Connection instanciaConexao;
 
+    // Credenciais do Clever Cloud
     private static final String URL = "jdbc:mysql://up4vyw7tvfazxwt0:eAhS6nnuRxmvnXn1QUDp@badhbeczpe2jfwpa7d5k-mysql.services.clever-cloud.com:3306/badhbeczpe2jfwpa7d5k";
     private static final String USUARIO = "up4vyw7tvfazxwt0";
     private static final String SENHA = "eAhS6nnuRxmvnXn1QUDp";
@@ -15,14 +16,18 @@ public class Conexao {
     private Conexao() {
     }
 
-    public static Connection getInstanciaConexao(){
-        try{
-        if (instanciaConexao == null) {
-            new Conexao();
-            instanciaConexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-        }
-    } catch (SQLException e) {
-            throw new RuntimeException(e);
+    // O "synchronized" impede colisão se duas requisições chegarem no mesmo milissegundo
+    public static synchronized Connection getInstanciaConexao() {
+        try {
+
+            if (instanciaConexao == null || instanciaConexao.isClosed() || !instanciaConexao.isValid(2)) {
+
+                instanciaConexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+                System.out.println("[DATABASE] Conexão com o Clever Cloud (MySQL) estabelecida.");
+            }
+        } catch (SQLException e) {
+            System.err.println("[DATABASE ERRO] O abismo recusou a conexão.");
+            throw new RuntimeException("Erro ao gerenciar conexão com o banco", e);
         }
         return instanciaConexao;
     }
